a9c3d3600ed4f4d20b8fd34484f4b03d189fba78
Tim Peeters <tim.peeters@me.com>
2018-06-23 20:27:01 +0200

    Add properties class.

---
+++ src/main/java/com/github/timpeeters/boot/shutdown/autoconfigure/ShutdownProperties.java
@@ -0,0 +1,7 @@
+package com.github.timpeeters.boot.shutdown.autoconfigure;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("shutdown")
+public class ShutdownProperties {
+}
