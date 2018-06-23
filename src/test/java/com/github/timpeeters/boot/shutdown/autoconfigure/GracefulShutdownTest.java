e1fc2dc1a7f245503b482e4b34ea614de7795100
Tim Peeters <tim.peeters@me.com>
2018-06-23 19:50:59 +0200

    First 200 ok test before trying to simulate the non-graceful shutdown.

--- src/test/java/com/github/timpeeters/boot/shutdown/autoconfigure/GracefulShutdownTest.java
+++ src/test/java/com/github/timpeeters/boot/shutdown/autoconfigure/GracefulShutdownTest.java
@@ -2,20 +2,45 @@ package com.github.timpeeters.boot.shutdown.autoconfigure;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class GracefulShutdownTest {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
     @Test
-    public void test() {
+    public void ok() {
+        RequestEntity request = new RequestEntity(HttpMethod.GET, URI.create("/"));
+
+        assertThat(restTemplate.exchange(request, String.class).getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 
     @SpringBootApplication
     public static class TestApplication {
+        @RestController
+        public class TestController {
+            @GetMapping("/")
+            public HttpStatus get() {
+                return HttpStatus.OK;
+            }
+        }
+
         public static void main(String[] args) {
             SpringApplication.run(TestApplication.class);
         }
