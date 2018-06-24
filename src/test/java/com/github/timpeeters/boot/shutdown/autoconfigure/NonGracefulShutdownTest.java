99c4cb615f18b8b1e0824010ce84c28e9dc9887e
Tim Peeters <tim.peeters@me.com>
2018-06-24 09:22:54 +0200

    Fix PMD issues.

--- src/test/java/com/github/timpeeters/boot/shutdown/autoconfigure/NonGracefulShutdownTest.java
+++ src/test/java/com/github/timpeeters/boot/shutdown/autoconfigure/NonGracefulShutdownTest.java
@@ -25,8 +25,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class NonGracefulShutdownTest {
     private static final Logger LOG = LoggerFactory.getLogger(NonGracefulShutdownTest.class);
 
-    private static final Semaphore requestReceived = new Semaphore(0);
-    private static final Semaphore requestFinished = new Semaphore(0);
+    private static final Semaphore REQ_RECEIVED = new Semaphore(0);
+    private static final Semaphore REQ_FINISHED = new Semaphore(0);
 
     private final int port = SocketUtils.findAvailableTcpPort();
 
@@ -50,7 +50,7 @@ public class NonGracefulShutdownTest {
     private void emulateSuccessfulRequest() throws ExecutionException, InterruptedException {
         ListenableFuture<ResponseEntity<HttpStatus>> slowRequest = sendRequestAndWaitForServerToStartProcessing();
 
-        requestFinished.release();
+        REQ_FINISHED.release();
 
         assertThat(slowRequest.get().getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
     }
@@ -59,7 +59,7 @@ public class NonGracefulShutdownTest {
         ListenableFuture<ResponseEntity<HttpStatus>> response =
                 new AsyncRestTemplate().getForEntity("http://localhost:" + port, HttpStatus.class);
 
-        requestReceived.acquire();
+        REQ_RECEIVED.acquire();
 
         return response;
     }
@@ -74,11 +74,11 @@ public class NonGracefulShutdownTest {
         public class TestController {
             @GetMapping("/")
             public ResponseEntity<?> get() throws InterruptedException {
-                requestReceived.release();
+                REQ_RECEIVED.release();
 
                 LOG.debug("Received get request, try to acquire semaphore");
 
-                requestFinished.acquire();
+                REQ_FINISHED.acquire();
 
                 LOG.debug("Semaphore acquired, returning empty response");
 
