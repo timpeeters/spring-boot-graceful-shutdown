0e5badae4fa40ae513f64a1161ad17782213c0f4
Tim Peeters <tim.peeters@me.com>
2018-06-24 09:16:39 +0200

    Finish test that illustrates the problem with a non graceful shutdown.

---
+++ src/test/java/com/github/timpeeters/boot/shutdown/autoconfigure/NonGracefulShutdownTest.java
@@ -0,0 +1,93 @@
+package com.github.timpeeters.boot.shutdown.autoconfigure;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.SocketUtils;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.AsyncRestTemplate;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Semaphore;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class NonGracefulShutdownTest {
+    private static final Logger LOG = LoggerFactory.getLogger(NonGracefulShutdownTest.class);
+
+    private static final Semaphore requestReceived = new Semaphore(0);
+    private static final Semaphore requestFinished = new Semaphore(0);
+
+    private final int port = SocketUtils.findAvailableTcpPort();
+
+    private ConfigurableApplicationContext applicationContext;
+
+    @Before
+    public void startSpringBootApplication() {
+        applicationContext = SpringApplication.run(TestApplication.class, "--server.port=" + port);
+    }
+
+    @Test
+    public void ok() throws ExecutionException, InterruptedException {
+        emulateSuccessfulRequest();
+        ListenableFuture<ResponseEntity<HttpStatus>> response = sendRequestAndWaitForServerToStartProcessing();
+
+        stopSpringBootApp();
+
+        assertThatThrownBy(response::get).hasCauseInstanceOf(IOException.class);
+    }
+
+    private void emulateSuccessfulRequest() throws ExecutionException, InterruptedException {
+        ListenableFuture<ResponseEntity<HttpStatus>> slowRequest = sendRequestAndWaitForServerToStartProcessing();
+
+        requestFinished.release();
+
+        assertThat(slowRequest.get().getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+    }
+
+    private ListenableFuture<ResponseEntity<HttpStatus>> sendRequestAndWaitForServerToStartProcessing() throws InterruptedException {
+        ListenableFuture<ResponseEntity<HttpStatus>> response =
+                new AsyncRestTemplate().getForEntity("http://localhost:" + port, HttpStatus.class);
+
+        requestReceived.acquire();
+
+        return response;
+    }
+
+    private void stopSpringBootApp() {
+        applicationContext.close();
+    }
+
+    @SpringBootApplication
+    public static class TestApplication {
+        @RestController
+        public class TestController {
+            @GetMapping("/")
+            public ResponseEntity<?> get() throws InterruptedException {
+                requestReceived.release();
+
+                LOG.debug("Received get request, try to acquire semaphore");
+
+                requestFinished.acquire();
+
+                LOG.debug("Semaphore acquired, returning empty response");
+
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+        }
+
+        public static void main(String[] args) {
+            SpringApplication.run(TestApplication.class);
+        }
+    }
+}
