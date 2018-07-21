29d530005c031962f49e5b6e4d0d8fe8082fca6e
Tim Peeters <tim.peeters@xplore.be>
2018-07-21 07:44:55 +0200

    Increase test coverage for exceptional cases.

--- src/test/java/com/github/timpeeters/boot/shutdown/autoconfigure/GracefulShutdownTomcatConnectorCustomizerTest.java
+++ src/test/java/com/github/timpeeters/boot/shutdown/autoconfigure/GracefulShutdownTomcatConnectorCustomizerTest.java
@@ -1,15 +1,46 @@
 package com.github.timpeeters.boot.shutdown.autoconfigure;
 
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.ProtocolHandler;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.core.task.SyncTaskExecutor;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class GracefulShutdownTomcatConnectorCustomizerTest {
-    private final GracefulShutdownTomcatConnectorCustomizer customizer =
-            new GracefulShutdownTomcatConnectorCustomizer(null, null);
+
+    @Mock
+    private ApplicationContext applicationContext;
+
+    @InjectMocks
+    private GracefulShutdownTomcatConnectorCustomizer customizer;
 
     @Test
     public void skipGracefulShutdownIfConnectorIsNull() {
-        assertThatCode(() -> customizer.contextClosed(null)).doesNotThrowAnyException();
+        assertThatCode(() -> customizer.contextClosed(new ContextClosedEvent(applicationContext)))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    public void differentThreadPoolImplementation() {
+        Connector mockConnector = mock(Connector.class);
+        ProtocolHandler mockProtocolHandler = mock(ProtocolHandler.class);
+
+        when(mockConnector.getProtocolHandler()).thenReturn(mockProtocolHandler);
+        when(mockProtocolHandler.getExecutor()).thenReturn(new SyncTaskExecutor());
+
+        customizer.customize(mockConnector);
+
+        assertThatCode(() -> customizer.contextClosed(new ContextClosedEvent(applicationContext)))
+                .doesNotThrowAnyException();
     }
 }
